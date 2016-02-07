4e5f0f030f0e080ed9d528ef645e3b31e602c628
Hugo Srevilled <hugo@xol.io>
2016-02-07 13:32:25 +0100

    2016.02.07 - added few events, iterators and start exposing worldgen

---
+++ src/io/xol/chunkstories/api/gui/Overlay.java
@@ -0,0 +1,39 @@
+package io.xol.chunkstories.api.gui;
+
+import io.xol.chunkstories.gui.OverlayableScene;
+
+//(c) 2015-2016 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
+public class Overlay
+{
+	public OverlayableScene mainScene;
+	public Overlay parent;
+
+	public Overlay(OverlayableScene scene, Overlay parent)
+	{
+		mainScene = scene;
+		this.parent = parent;
+	}
+
+	public void drawToScreen(int x, int y, int w, int h)
+	{
+		
+	}
+
+	public boolean handleKeypress(int k)
+	{
+		return false;
+	}
+
+	public boolean onClick(int posx, int posy, int button)
+	{
+		return false;
+	}
+	
+	public boolean onScroll(int dy)
+	{
+		return false;
+	}
+}
---
+++ src/io/xol/chunkstories/api/world/ChunksIterator.java
@@ -0,0 +1,19 @@
+package io.xol.chunkstories.api.world;
+
+import java.util.Iterator;
+
+import io.xol.chunkstories.world.CubicChunk;
+
+//(c) 2015-2016 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
+/**
+ * An Iterator used specifically for chunks, returned by some objects
+ * @author Hugo
+ *
+ */
+public interface ChunksIterator extends Iterator<CubicChunk>
+{
+
+}
