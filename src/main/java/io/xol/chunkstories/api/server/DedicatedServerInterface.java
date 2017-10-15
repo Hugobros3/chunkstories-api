79f82e1311807788ce32ba680b0a53c47b813d15
Gobrosse <Gobrosse@Xolioware-Ryzen.lan>
2017-10-15 20:19:07 +0200

    Add a specific interface for dedicated servers

---
+++ src/main/java/io/xol/chunkstories/api/server/DedicatedServerInterface.java
@@ -0,0 +1,11 @@
+package io.xol.chunkstories.api.server;
+
+//(c) 2015-2017 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
+public interface DedicatedServerInterface extends ServerInterface {
+	
+	/** Stops the server. */
+	public void stop();
+}
