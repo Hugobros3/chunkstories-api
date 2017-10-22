b75b6122db2d03f76dc7bf12a0bfe5586491ef0f
Gobrosse <hugo@xol.io>
2017-10-22 15:30:13 +0200

    Added a specific event for core content to hook in

---
+++ src/main/java/io/xol/chunkstories/api/events/item/EventItemDroppedToWorld.java
@@ -0,0 +1,78 @@
+package io.xol.chunkstories.api.events.item;
+
+import io.xol.chunkstories.api.Location;
+import io.xol.chunkstories.api.entity.Entity;
+import io.xol.chunkstories.api.events.CancellableEvent;
+import io.xol.chunkstories.api.events.EventListeners;
+import io.xol.chunkstories.api.events.entity.EntityDeathEvent;
+import io.xol.chunkstories.api.item.inventory.Inventory;
+import io.xol.chunkstories.api.item.inventory.ItemPile;
+
+//(c) 2015-2017 XolioWare Interactive
+//http://chunkstories.xyz
+//http://xol.io
+
+public class EventItemDroppedToWorld extends CancellableEvent
+{
+	// Every event class has to have this
+	
+	static EventListeners listeners = new EventListeners(EntityDeathEvent.class);
+	
+	@Override
+	public EventListeners getListeners()
+	{
+		return listeners;
+	}
+	
+	public static EventListeners getListenersStatic()
+	{
+		return listeners;
+	}
+	
+	// Specific event code
+	
+	private Location location;
+	private Inventory inventoryFrom;
+	private ItemPile itemPile;
+	
+	/** The entity to be added to the world that will represent this item pile */
+	private Entity itemEntity;
+
+	public EventItemDroppedToWorld(Location location, Inventory inventoryFrom, ItemPile itemPile) {
+		this.location = location;
+		this.inventoryFrom = inventoryFrom;
+		this.itemPile = itemPile;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public Inventory getInventoryFrom() {
+		return inventoryFrom;
+	}
+
+	public void setInventoryFrom(Inventory inventoryFrom) {
+		this.inventoryFrom = inventoryFrom;
+	}
+
+	public ItemPile getItemPile() {
+		return itemPile;
+	}
+
+	public void setItemPile(ItemPile itemPile) {
+		this.itemPile = itemPile;
+	}
+	
+	public Entity getItemEntity() {
+		return itemEntity;
+	}
+
+	public void setItemEntity(Entity itemEntity) {
+		this.itemEntity = itemEntity;
+	}
+}
