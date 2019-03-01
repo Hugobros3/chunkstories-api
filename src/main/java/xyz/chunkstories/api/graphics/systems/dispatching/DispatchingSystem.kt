c8907badb18f74eee50f26fe962a144b1ca25bac
Gobrosse <hugo@xol.io>
2019-03-01 11:31:23 +0100

    added named string to help with implem

--- src/main/java/xyz/chunkstories/api/graphics/systems/dispatching/DispatchingSystem.kt
+++ src/main/java/xyz/chunkstories/api/graphics/systems/dispatching/DispatchingSystem.kt
@@ -14,16 +14,19 @@ import xyz.chunkstories.api.graphics.systems.GraphicSystem
 /** A Dispatching system is a system that has the ability to dispatch draw commands in potentially multiple passes.
  * The passes do not get to choose whether or not they receive the input vertex data. */
 interface DispatchingSystem<T : Representation> : GraphicSystem {
+    val representationName: String
 }
 
 interface RepresentationsGobbler<T : Representation> {
     //TODO val frame: Frame
     //supply some
-    val cameras: Array<PassInstance>
+    val passInstances: Array<PassInstance>
 
     fun acceptRepresentation(representation: T, cameraVisibility: Int = -1)
 }
 
 interface RepresentationsProvider<T: Representation> {
     fun gatherRepresentations(representationsGobbler: RepresentationsGobbler<T>)
+
+    val representationName: String
 }
