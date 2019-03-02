93f295ba154215b6f07af47e3c8dd7545526765a
Gobrosse <hugo@xol.io>
2019-03-02 18:38:35 +0100

    made modelinstances more customizable

--- src/main/java/xyz/chunkstories/api/graphics/representation/Model.kt
+++ src/main/java/xyz/chunkstories/api/graphics/representation/Model.kt
@@ -7,18 +7,21 @@
 package xyz.chunkstories.api.graphics.representation
 
 import org.joml.Matrix4f
-import org.joml.Vector3dc
-import org.joml.Vector3fc
 import xyz.chunkstories.api.animation.Animation
+import xyz.chunkstories.api.animation.Animator
 import xyz.chunkstories.api.graphics.Mesh
-import xyz.chunkstories.api.graphics.structs.IgnoreGLSL
+import xyz.chunkstories.api.graphics.MeshMaterial
 import xyz.chunkstories.api.graphics.structs.InterfaceBlock
 
 data class Model(val meshes: List<Mesh>)
 
 /** One instance of a particular model */
-
-data class ModelInstance(val model: Model, val position: ModelPosition) : Representation {
+data class ModelInstance(val model: Model,
+                         val position: ModelPosition,
+                         val materials: Map<Int, MeshMaterial> = emptyMap(),
+                         val meshesMask: Int = -1,
+                         val animator: Animator? = null
+) : Representation {
 }
 
-data class ModelPosition(val matrix: Matrix4f = Matrix4f()): InterfaceBlock
+data class ModelPosition(val matrix: Matrix4f = Matrix4f()) : InterfaceBlock
