f97dc186fcc2ac78114e55743c9b3196fd7e05c8
Gobrosse <hugo@xol.io>
2019-03-03 18:04:00 +0100

    added animations stuff

--- src/main/java/xyz/chunkstories/api/graphics/systems/dispatching/ModelsRenderer.kt
+++ src/main/java/xyz/chunkstories/api/graphics/systems/dispatching/ModelsRenderer.kt
@@ -1,8 +1,8 @@
 package xyz.chunkstories.api.graphics.systems.dispatching
 
-import xyz.chunkstories.api.graphics.representation.ModelInstance
-
 interface ModelsRenderer : DispatchingSystem {
-    var shader: String
     var materialTag: String
+
+    var shader: String
+    var supportsAnimations: Boolean
 }
---
+++ src/main/java/xyz/chunkstories/api/graphics/systems/drawing/SkyDrawer.kt
@@ -0,0 +1,2 @@
+package xyz.chunkstories.api.graphics.systems.drawing
+
