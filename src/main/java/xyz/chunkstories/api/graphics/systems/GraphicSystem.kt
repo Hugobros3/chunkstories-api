61878d45d6cf9ff81e9d72697fcd51afd7c7309c
Hugo 'Gobrosse' Devillers <hugo@xol.io>
2019-04-24 18:23:14 +0200

    added system setup {} to api

--- src/main/java/xyz/chunkstories/api/graphics/systems/GraphicSystem.kt
+++ src/main/java/xyz/chunkstories/api/graphics/systems/GraphicSystem.kt
@@ -1,3 +1,5 @@
 package xyz.chunkstories.api.graphics.systems
 
+import xyz.chunkstories.api.graphics.rendergraph.SystemExecutionContext
+
 interface GraphicSystem
--- src/main/java/xyz/chunkstories/api/graphics/systems/drawing/DrawingSystem.kt
+++ src/main/java/xyz/chunkstories/api/graphics/systems/drawing/DrawingSystem.kt
@@ -6,10 +6,12 @@
 
 package xyz.chunkstories.api.graphics.systems.drawing
 
+import xyz.chunkstories.api.graphics.rendergraph.SystemExecutionContext
 import xyz.chunkstories.api.graphics.systems.GraphicSystem
 
 /** A Drawing system is a system that supplies vertex data and submits draw calls in the taskInstance
  * of whatever passes invoke it. */
 interface DrawingSystem : GraphicSystem {
-
+    /** Registers per-frame setup code, to be executed at frame graph creation time, before any rendering */
+    fun setup(dslCode: SystemExecutionContext.() -> Unit)
 }
