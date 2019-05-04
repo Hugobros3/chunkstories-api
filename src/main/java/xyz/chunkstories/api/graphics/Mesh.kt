4004ed26d0c96c90f0ea13968c88fb00706b29ae
Gobrosse <hugo@xol.io>
2019-05-04 15:34:08 +0200

    utility method to flip meshes winding order

--- src/main/java/xyz/chunkstories/api/graphics/Mesh.kt
+++ src/main/java/xyz/chunkstories/api/graphics/Mesh.kt
@@ -7,6 +7,7 @@
 package xyz.chunkstories.api.graphics
 
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 /**
  *
@@ -21,3 +22,30 @@ data class MeshAttributeSet(val name: String, val components: Int, val format: V
 }
 
 data class MeshMaterial(val name: String, val textures: Map<String, String>, val tag: String = "opaque")
+
+fun Mesh.reverseWindingOrder() : Mesh {
+    val reversedAttributes = attributes.map {
+        val reversedByteBuffer = ByteBuffer.allocateDirect(it.data.capacity())
+
+        val bytesPerVertex = it.format.bytesPerComponent * it.components
+
+        for(i in 0 until it.data.capacity() / bytesPerVertex) {
+            val ifl = (i / 3) * 3
+            //val irr = 0
+
+            val t = if(i % 3 == 0) ifl else if(i % 3 == 1) ifl + 2 else ifl + 1
+
+            val tb = t * bytesPerVertex
+            for(b in 0 until bytesPerVertex) {
+                reversedByteBuffer.put(it.data.get(tb + b))
+            }
+        }
+        //reversedByteBuffer.flip()
+
+        reversedByteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+
+        MeshAttributeSet(it.name, it.components, it.format, reversedByteBuffer)
+    }
+
+    return Mesh(vertices, reversedAttributes, material, boneIds)
+}
