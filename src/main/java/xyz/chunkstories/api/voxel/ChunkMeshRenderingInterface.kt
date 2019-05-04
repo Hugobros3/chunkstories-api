db6688467cfc44de95addcb32b2982af656f335c
Gobrosse <hugo@xol.io>
2019-05-04 15:34:25 +0200

    block models can take a transformation matrix

--- src/main/java/xyz/chunkstories/api/voxel/ChunkMeshRenderingInterface.kt
+++ src/main/java/xyz/chunkstories/api/voxel/ChunkMeshRenderingInterface.kt
@@ -1,9 +1,10 @@
 package xyz.chunkstories.api.voxel
 
+import org.joml.Matrix4f
 import org.joml.Vector3fc
 import xyz.chunkstories.api.graphics.MeshMaterial
 import xyz.chunkstories.api.graphics.representation.Model
 
 interface ChunkMeshRenderingInterface {
-    fun addModel(model: Model, offset: Vector3fc? = null, materialsOverrides: Map<Int, MeshMaterial> = emptyMap())
+    fun addModel(model: Model, transformation: Matrix4f? = null, materialsOverrides: Map<Int, MeshMaterial> = emptyMap())
 }
