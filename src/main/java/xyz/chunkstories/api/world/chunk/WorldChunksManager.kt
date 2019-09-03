43ca75756430787e948255761fab64ac18f89d7a
Gobrosse <hugo@xol.io>
2019-09-03 11:18:52 +0200

    fix spotless

--- src/main/java/xyz/chunkstories/api/world/chunk/WorldChunksManager.kt
+++ src/main/java/xyz/chunkstories/api/world/chunk/WorldChunksManager.kt
@@ -1,3 +1,9 @@
+//
+// This file is a part of the Chunk Stories API codebase
+// Check out README.md for more information
+// Website: http://chunkstories.xyz
+//
+
 package xyz.chunkstories.api.world.chunk
 
 import xyz.chunkstories.api.Location
